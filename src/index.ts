1224004ecd675bfbe51df267ac43aa49252678d4
EDubov <Edubov@project.ibs.ru>
2026-04-08 15:12:33 +0500

    finalize create classes

--- src/index.ts
+++ src/index.ts
@@ -5,8 +5,8 @@ import express from "express";
 import cors from "cors";
 
 import subjectsRouter from "./routes/subjects.js";
-// import usersRouter from "./routes/users.js";
-// import classesRouter from "./routes/classes.js";
+import usersRouter from "./routes/users.js";
+import classesRouter from "./routes/classes.js";
 import securityMiddleware from "./middleware/security.js";
 import { toNodeHandler } from "better-auth/node";
 import { auth } from "./lib/auth.js";
@@ -31,8 +31,8 @@ app.use(express.json());
 app.use(securityMiddleware);
 
 app.use("/api/subjects", subjectsRouter);
-// app.use("/api/users", usersRouter);
-// app.use("/api/classes", classesRouter);
+app.use("/api/users", usersRouter);
+app.use("/api/classes", classesRouter);
 
 app.get("/", (req, res) => {
   res.send("Hello, welcome to the Classroom API!");
